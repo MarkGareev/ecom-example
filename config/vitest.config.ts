82846a9bb67130c8b220712602c7c79aff3261e8
Mark Gareev <gareevmark02@gmail.com>
2026-06-19 18:38:23 +0400

    test: exclude tests/ui from server vitest config

--- config/vitest.config.ts
+++ config/vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     environment: 'node',
     globals: true,
     setupFiles: ['./tests/setup.ts'],
+    exclude: ['tests/ui/**', 'node_modules/**'],
   },
   resolve: {
     alias: {
